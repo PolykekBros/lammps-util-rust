9058472d907ef3b02844d3ccc24aad9743917dd2
Denis Strizhkin <strdenis02@gmail.com>
2024-11-03 02:52:45 +0300

    simplify expressions

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,14 @@ use std::io;
 use std::path::Path;
 
 // use memmap2::Mmap;
+const HEADER_TIMESTEP = "ITEM: TIMESTEP";
+const HEADER_NUM_OF_ATOMS = "ITEM: NUMBER OF ATOMS";
+const HEADER_SYM_BOX = "ITEM: BOX";
+const HEADER_ATOMS = "ITEM: ATOMS";
 
 pub struct DumpTimestep {
     start: usize,
+    pub number: u64,
     pub atom_count: usize,
 }
 
@@ -18,20 +23,24 @@ pub struct Dump {
 
 #[derive(Debug)]
 pub enum DumpParsingError {
-    NoTimestep,
-    NoNumberOfAtoms,
+    InvalidOrMissingTimestep,
+    InvalidOrMissingNumberOfAtoms,
+    MissingSymBox,
+    MissingAtomKeys,
     DuplicateKeys,
     DuplicateTimesteps,
-    IO(io::Error),
-    InvalidNumber,
     MissingAtomRow,
     InvalidAtomRow,
+    IO(io::Error),
 }
 
 impl Dump {
     pub fn new(path: &Path, timesteps: &[u64]) -> Result<Self, DumpParsingError> {
         let lines = fs::read_to_string(path).map_err(DumpParsingError::IO)?;
-        let mut lines = lines.split("\n");
+        let mut lines = lines.trim().split("\n");
+
+        let mut timesteps = timesteps.to_vec();
+        timesteps.sort();
 
         let mut dump = Self {
             atoms: Vec::new(),
@@ -39,76 +48,81 @@ impl Dump {
             keys: HashMap::new(),
         };
 
-        let mut timestep: Option<u64> = None;
-        let mut atom_count: Option<usize> = None;
-        while let Some(line) = lines.next() {
-            if line == "ITEM: TIMESTEP" {
-                timestep = Some(
-                    lines
-                        .next()
-                        .ok_or(DumpParsingError::NoTimestep)?
-                        .parse::<u64>()
-                        .map_err(|_| DumpParsingError::InvalidNumber)?,
-                );
-            } else if line == "ITEM: NUMBER OF ATOMS" {
-                atom_count = Some(
-                    lines
-                        .next()
-                        .ok_or(DumpParsingError::NoNumberOfAtoms)?
-                        .parse::<usize>()
-                        .map_err(|_| DumpParsingError::InvalidNumber)?,
-                );
-            } else if line.contains("ITEM: ATOMS") {
-                if dump.keys.is_empty() {
-                    let keys = line.split_whitespace().skip(2);
-                    for key in keys {
-                        if dump.keys.contains_key(key) {
-                            return Err(DumpParsingError::DuplicateKeys);
-                        }
-                        dump.keys.insert(key.to_string(), dump.keys.len());
-                    }
-                }
-                let Some(timestep) = timestep else {
-                    return Err(DumpParsingError::NoTimestep);
-                };
-                let Some(atom_count) = atom_count else {
-                    return Err(DumpParsingError::NoNumberOfAtoms);
-                };
+        'read_all: loop {
+            let timestep = match (lines.next(), lines.next().map(str::parse::<u64>)) {
+                (Some(HEADER_TIMESTEP), Some(Ok(n))) => n,
+                (None, _) => break Ok(dump),
+                (_, _) => break Err(DumpParsingError::InvalidOrMissingTimestep),
+            };
+            let number_of_atoms = match (lines.next(), lines.next().map(str::parse::<usize>)) {
+                (Some(HEADER_NUM_OF_ATOMS), Some(Ok(n))) => n,
+                (_, _) => break Err(DumpParsingError::InvalidOrMissingNumberOfAtoms),
+            };
+            if !timesteps.is_empty() {
                 if &timestep > timesteps.last().unwrap_or(&u64::MAX) {
-                    break;
-                }
-                if timesteps.is_empty() || timesteps.contains(&timestep) {
-                    if dump.timesteps.contains_key(&timestep) {
-                        return Err(DumpParsingError::DuplicateTimesteps);
-                    }
-                    dump.timesteps.insert(
-                        timestep,
-                        DumpTimestep {
-                            start: atom_count * dump.timesteps.len() * dump.keys.len(),
-                            atom_count,
-                        },
-                    );
-                    let mut atoms = vec![0.0; atom_count * dump.keys.len()];
-                    for i in 0..atom_count {
-                        let Some(tokens) = lines.next() else {
-                            return Err(DumpParsingError::MissingAtomRow);
-                        };
-                        let tokens: Vec<&str> = tokens.split_whitespace().collect();
-                        if tokens.len() != dump.keys.len() {
-                            return Err(DumpParsingError::InvalidAtomRow);
-                        }
-                        for j in dump.keys.values() {
-                            atoms[atom_count * j + i] = tokens[*j]
-                                .parse::<f64>()
-                                .map_err(|_| DumpParsingError::InvalidNumber)?
+                    break Ok(dump);
+                } else if !timesteps.contains(&timestep) {
+                    for _ in 0..number_of_atoms + 4 + 1 {
+                        if lines.next().is_none() {
+                            break;
                         }
                     }
-                    dump.atoms.extend_from_slice(&atoms);
+                    continue;
                 }
             }
-        }
+            match lines.next().map(|l| l.split_at_checked(HEADER_SYM_BOX.len())).flatten() {
+                Some((HEADER_SYM_BOX, _)) => {
+                    // TODO: read box parameters
+                    lines.next();
+                    lines.next();
+                    lines.next();
+                }
+                _ => break Err(DumpParsingError::MissingSymBox),
+            }
+            match lines.next().map(|l| l.split_at_checked(HEADER_ATOMS.len())).flatten() {
+                Some((HEADER_ATOMS, keys)) => {
+                    if dump.keys.is_empty() {
+                        for key in keys.split_whitespace() {
+                            if dump.keys.insert(key.to_string(), dump.keys.len()).is_some() {
+                                break 'read_all Err(DumpParsingError::DuplicateKeys);
+                            }
+                        }
+                    }
+                }
+                _ => break Err(DumpParsingError::MissingAtomKeys),
+            }
+            if dump
+                .timesteps
+                .insert(
+                    timestep,
+                    DumpTimestep {
+                        start: number_of_atoms * dump.timesteps.len() * dump.keys.len(),
+                        number: timestep,
+                        atom_count: number_of_atoms,
+                    },
+                )
+                .is_some()
+            {
+                break Err(DumpParsingError::DuplicateKeys);
+            }
 
-        Ok(dump)
+            let mut atoms = vec![0.0; number_of_atoms * dump.keys.len()];
+            for i in 0..number_of_atoms {
+                let Some(tokens) = lines.next() else {
+                    break 'read_all Err(DumpParsingError::MissingAtomRow);
+                };
+                let tokens: Vec<&str> = tokens.split_whitespace().collect();
+                if tokens.len() != dump.keys.len() {
+                    return Err(DumpParsingError::InvalidAtomRow);
+                }
+                for j in dump.keys.values() {
+                    atoms[number_of_atoms * j + i] = tokens[*j]
+                        .parse::<f64>()
+                        .map_err(|_| DumpParsingError::InvalidAtomRow)?
+                }
+            }
+            dump.atoms.extend_from_slice(&atoms);
+        }
     }
 
     pub fn get_property(&self, timestep: u64, key: &str) -> &[f64] {
