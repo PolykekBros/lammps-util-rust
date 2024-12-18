56d98a4db44647b487981a4236c1a585dadb7712
Denis Strizhkin <strdenis02@gmail.com>
2024-12-18 21:46:16 +0300

    crater analysis improve performance

--- crater-analysis/src/main.rs
+++ crater-analysis/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use core::error;
 use lammps_util_rust::dump_file::DumpFile;
-use lammps_util_rust::dump_snapshot::DumpSnapshot;
+use lammps_util_rust::dump_snapshot::{DumpSnapshot, SymBox};
 use std::collections::HashMap;
 use std::io;
 use std::path::Path;
@@ -30,8 +30,11 @@ struct Cli {
 }
 
 struct Stripes<'a> {
-    ids: Vec<Vec<usize>>,
-    snap: &'a DumpSnapshot,
+    indices: Vec<Vec<usize>>,
+    sym_box: SymBox,
+    x: &'a [f64],
+    y: &'a [f64],
+    z: &'a [f64],
 }
 
 fn check_cutoff(a: (f64, f64, f64), b: (f64, f64, f64), cutoff: f64) -> bool {
@@ -43,59 +46,70 @@ fn check_cutoff(a: (f64, f64, f64), b: (f64, f64, f64), cutoff: f64) -> bool {
 
 impl<'a> Stripes<'a> {
     pub fn new(snap: &'a DumpSnapshot, zero_lvl: f64, max_depth: f64, width: f64) -> Self {
-        let snap_z = snap.get_property("z");
+        assert!(max_depth >= 0.0);
+        assert!(width >= 0.0);
+        let x = snap.get_property("x");
+        let y = snap.get_property("y");
+        let z = snap.get_property("z");
         let count = (max_depth / width).ceil() as usize;
-        let mut ids = Vec::new();
-        for _ in 0..count {
-            ids.push(Vec::new());
+        let indices = (0..count).fold(Vec::new(), |mut indices, _| {
+            indices.push(Vec::new());
+            indices
+        });
+        let indices = z
+            .iter()
+            .copied()
+            .enumerate()
+            .fold(indices, |mut indices, (i, z)| {
+                if zero_lvl - max_depth < z && z <= zero_lvl {
+                    let j = ((zero_lvl - z) / width).floor() as usize;
+                    indices[j].push(i);
+                }
+                indices
+            });
+        Self {
+            indices,
+            sym_box: snap.sym_box.clone(),
+            x,
+            y,
+            z,
         }
-        for (i, z) in snap_z.iter().copied().enumerate() {
-            if zero_lvl >= z && zero_lvl - max_depth < z {
-                let j = ((zero_lvl - z) / width).floor() as usize;
-                ids[j].push(i);
-            }
-        }
-        Self { ids, snap }
     }
 
     #[inline]
     pub fn len(&self) -> usize {
-        self.ids.len()
+        self.indices.len()
     }
 
     #[inline]
     pub fn get_xyz(&self, i: usize) -> (f64, f64, f64) {
-        (
-            self.snap.get_property("x")[i],
-            self.snap.get_property("y")[i],
-            self.snap.get_property("z")[i],
-        )
+        (self.x[i], self.y[i], self.z[i])
     }
 
-    pub fn get_missing_indexes(&self, snap: &Stripes, i: usize, cutoff: f64) -> Vec<usize> {
-        let mut indexes = Vec::new();
-        for self_i in self.ids[i].iter().copied() {
-            let self_xyz = self.get_xyz(self_i);
-            if self_xyz.0 <= self.snap.sym_box.xlo + cutoff
-                || self_xyz.0 >= self.snap.sym_box.xhi - cutoff
-                || self_xyz.1 <= self.snap.sym_box.ylo + cutoff
-                || self_xyz.1 >= self.snap.sym_box.yhi - cutoff
-            {
-                continue;
-            }
-            let mut missing = true;
-            for snap_i in snap.ids[i].iter().copied() {
-                let snap_xyz = snap.get_xyz(snap_i);
-                if check_cutoff(self_xyz, snap_xyz, cutoff) {
-                    missing = false;
-                    break;
+    pub fn get_missing_indices(&self, snap: &Stripes, i: usize, cutoff: f64) -> Vec<usize> {
+        self.indices[i]
+            .iter()
+            .copied()
+            .fold(Vec::new(), |mut indices, self_i| {
+                let self_xyz = self.get_xyz(self_i);
+                if self.sym_box.xlo + cutoff < self_xyz.0
+                    && self_xyz.0 < self.sym_box.xhi - cutoff
+                    && self.sym_box.ylo + cutoff < self_xyz.1
+                    && self_xyz.1 < self.sym_box.yhi - cutoff
+                {
+                    let missing = snap.indices[i]
+                        .iter()
+                        .copied()
+                        .fold(true, |missing, snap_i| {
+                            let snap_xyz = snap.get_xyz(snap_i);
+                            missing && !check_cutoff(self_xyz, snap_xyz, cutoff)
+                        });
+                    if missing {
+                        indices.push(self_i);
+                    }
                 }
-            }
-            if missing {
-                indexes.push(self_i);
-            }
-        }
-        indexes
+                indices
+            })
     }
 }
 
@@ -103,18 +117,17 @@ fn get_crater_candidate_indecies(
     snap_input: &DumpSnapshot,
     snap_final: &DumpSnapshot,
     max_depth: f64,
-    threshold: f64,
+    cutoff: f64,
     width: f64,
 ) -> Vec<usize> {
     let zero_lvl = snap_input.get_zero_lvl();
     let stripes_input = Stripes::new(snap_input, zero_lvl, max_depth, width);
     let stripes_final = Stripes::new(snap_final, zero_lvl, max_depth, width);
-    let mut crater_indexes = Vec::new();
-    for i in 0..stripes_input.len() {
-        let indexes = stripes_input.get_missing_indexes(&stripes_final, i, threshold);
-        crater_indexes.extend(indexes);
-    }
-    crater_indexes
+    (0..stripes_input.len()).fold(Vec::new(), |mut indices, i| {
+        let stripe_indices = stripes_input.get_missing_indices(&stripes_final, i, cutoff);
+        indices.extend(stripe_indices);
+        indices
+    })
 }
 
 fn save_crater_candidates(
