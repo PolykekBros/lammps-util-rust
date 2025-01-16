582b84c4c9c01a7342c3415827e3e007b965d353
Denis Strizhkin <strdenis02@gmail.com>
2025-01-16 16:05:09 +0300

    component shift fix

--- component-shift/src/main.rs
+++ component-shift/src/main.rs
@@ -1,11 +1,11 @@
-use std::collections::HashMap;
 use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::Parser;
 use itertools::izip;
-use lammps_util_rust::{DumpFile, DumpSnapshot, XYZ, clusterize_snapshot};
-use nalgebra::{Point3, Vector2, Vector3};
+use lammps_util_rust::{crater_snapshot, DumpFile, DumpSnapshot, XYZ};
+use log::debug;
+use nalgebra::Vector3;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -17,15 +17,9 @@ struct Cli {
     dump_final: PathBuf,
 }
 
-fn calc_shift(
-    a: &[XYZ],
-    a_id: &[usize],
-    b: &[XYZ],
-    b_map: &HashMap<usize, usize>,
-) -> (usize, Vector3<f64>, Vector3<f64>) {
-    let deltas = izip!(a, a_id)
-        .filter(|(_, a_id)| b_map.contains_key(*a_id))
-        .map(|(a, a_id)| **a - *b[b_map[a_id]])
+fn get_coords_shift(a: &[XYZ], b: &[XYZ]) -> (usize, Vector3<f64>, Vector3<f64>) {
+    let deltas = izip!(a, b)
+        .map(|(a, b)| **b - **a)
         .collect::<Vec<Vector3<f64>>>();
     let sum = deltas.iter().copied().sum::<Vector3<f64>>();
     let sum2 = deltas
@@ -33,33 +27,54 @@ fn calc_shift(
         .copied()
         .map(|a| a.component_mul(&a))
         .sum::<Vector3<f64>>();
-    (sum.len(), sum, sum2)
+    (deltas.len(), sum, sum2)
 }
 
-fn get_properties(input_snapshot: &DumpSnapshot, final_snapshot: &DumpSnapshot) -> (f64, f64, f64) {
-    let input_coords = input_snapshot.get_coordinates();
-    let input_ids = input_snapshot.get_property("id");
-    let final_coords = final_snapshot.get_coordinates();
+fn get_ids(input_snapshot: &DumpSnapshot, final_snapshot: &DumpSnapshot) -> Vec<f64> {
+    let crater_snapshot = crater_snapshot(input_snapshot, final_snapshot, 3.0);
     let final_ids = final_snapshot.get_property("id");
-    let cluster_snapshot
-    let f_map = final_ids
+    let crater_ids = crater_snapshot.get_property("id");
+    let ids = crater_ids
         .iter()
+        .filter(|id| final_ids.contains(id))
         .copied()
-        .enumerate()
-        .fold(HashMap::new(), |mut map, (i, id)| {
-            map.insert(id as usize, i);
-            map
-        });
-    todo!()
+        .collect::<Vec<_>>();
+    debug!("ids.len: {}", ids.len());
+    ids
+}
+
+fn get_coords_filtered(snapshot: &DumpSnapshot, ids: &[f64]) -> Vec<XYZ> {
+    izip!(snapshot.get_coordinates(), snapshot.get_property("id"))
+        .filter(|(_, id)| ids.contains(id))
+        .map(|(xyz, _)| xyz)
+        .collect()
+}
+
+fn get_coords(
+    input_snapshot: &DumpSnapshot,
+    final_snapshot: &DumpSnapshot,
+) -> (Vec<XYZ>, Vec<XYZ>) {
+    let ids = get_ids(input_snapshot, final_snapshot);
+    (
+        get_coords_filtered(input_snapshot, &ids),
+        get_coords_filtered(final_snapshot, &ids),
+    )
 }
 
 fn main() -> Result<()> {
+    env_logger::init();
     let cli = Cli::parse();
+
     let dump_initial = DumpFile::read(&cli.dump_initial, &[])?;
-    let dump_initial_snapshot = dump_initial.get_snapshots()[0];
+    let initial_snapshot = dump_initial.get_snapshots()[0];
+
     let dump_final = DumpFile::read(&cli.dump_final, &[])?;
-    let dump_final_snapshot = dump_final.get_snapshots()[0];
-    // let (rms_x, rms_y, rms_z) = calc_rms_all_components(dump_initial_snapshot, dump_final_snapshot);
-    // println!("{rms_x:.6} {rms_y:.6} {rms_z:.6}");
+    let final_snapshot = dump_final.get_snapshots()[0];
+
+    let coords = get_coords(initial_snapshot, final_snapshot);
+    let (cnt, sum, sum2) = get_coords_shift(&coords.0, &coords.1);
+    println!("{cnt}");
+    println!("{} {} {}", sum.x, sum.y, sum.z);
+    println!("{} {} {}", sum2.x, sum2.y, sum2.z);
     Ok(())
 }
