204f887aedf1984afb47a0332cef8483885e5edd
Denis Strizhkin <strdenis02@gmail.com>
2025-02-26 16:25:49 +0300

    single data

--- rim-analysis/src/main.rs
+++ rim-analysis/src/main.rs
@@ -1,11 +1,12 @@
-use anyhow::{anyhow, bail, Context, Result};
-use clap::Parser;
+use anyhow::{bail, Context, Result};
+use clap::{Parser, Subcommand};
 use lammps_util_rust::{
     clusterize_snapshot, copy_snapshot_with_indices, get_cluster_counts, DumpFile, DumpSnapshot,
 };
 use log::info;
 use nalgebra::{vector, Vector2};
 use std::{
+    array::from_fn,
     collections::HashSet,
     fs::File,
     io::{BufRead, BufReader},
@@ -13,25 +14,35 @@ use std::{
     str::FromStr,
 };
 
-const RIM_THRESHOLD: usize = 50;
+const RIM_THRESHOLD: usize = 10;
 const CLUSTER_TRAJECTORY_LINE: usize = 38;
 const ANGLE_ROTATION: usize = 10;
+const DATA_LEN: usize = 360 / ANGLE_ROTATION;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// DUMP INPUT
-    dump_input_file: PathBuf,
-
-    /// DUMP FINAL
-    dump_final_file: PathBuf,
-
-    /// OUTPUT DIR
-    output_dir: PathBuf,
-
     /// Cutoff (A)
     #[arg(short, long, default_value_t = 3.0)]
     cutoff: f64,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Analyse a single run dir
+    Single {
+        /// Run directory
+        dir: PathBuf,
+    },
+
+    /// Analyse a whole results dir
+    All {
+        /// Results directory
+        dir: PathBuf,
+    },
 }
 
 fn get_above_zero(snap: &DumpSnapshot, zero_lvl: f64) -> DumpSnapshot {
@@ -87,60 +98,80 @@ fn get_center_pos(cluster_xyz_path: &Path) -> Result<Vector2<f64>> {
     let Some((Ok(x), Ok(y))) = iter.next().zip(iter.next()) else {
         bail!("Failed to parse XY coordinates in the line {CLUSTER_TRAJECTORY_LINE}");
     };
-    vector![x, y];
+    Ok(vector![x, y])
 }
 
 fn angle_between_vectors(a: Vector2<f64>, b: Vector2<f64>) -> f64 {
-    let dot = a.dot(&b);
-    let a_len = a.norm();
-    let b_len = b.norm();
-    let cos_theta = dot / (a_len * b_len);
-    let cos_theta = cos_theta.clamp(-1.0, 1.0);
-    let angle_radians = cos_theta.acos();
-    let angle_degrees = angle_radians * 180.0 / PI;
-    angle_degrees
+    let angle = a.y.atan2(a.x) - b.y.atan2(b.x);
+    angle.to_degrees() + 180.0
 }
 
-fn get_angle_distribution(
-    snap: &DumpSnapshot,
-    center: Vector2<f64>,
-) -> (Vec<Vec<f64>>, Vec<usize>) {
-    let start = vector![0, -1];
-    let mut radii = vec![Vec::new(); 360 / ANGLE_ROTATION];
-    let mut count = vec![0; 360 / ANGLE_ROTATION];
+fn get_angle_distribution(snap: &DumpSnapshot, center: Vector2<f64>) -> [Vec<f64>; DATA_LEN] {
+    let start = vector![0.0, -1.0];
+    let mut radii = from_fn(|_| Vec::new());
     for coord in snap
         .get_coordinates()
         .into_iter()
         .map(|xyz| vector![xyz.x, xyz.y] - center)
     {
-        let angle = start.angle(&coord).to_degrees();
-        let angle = (angle + 360.0) % 360.0;
+        let angle = angle_between_vectors(start, coord);
         let index = (angle / ANGLE_ROTATION as f64) as usize;
         let radius = coord.magnitude();
-        count[index] += 1;
         radii[index].push(radius);
     }
-    (radii, count)
+    radii
+}
+
+fn get_mean(values: &[f64]) -> f64 {
+    values.iter().sum::<f64>() / values.len() as f64
+}
+
+fn get_std(values: &[f64]) -> f64 {
+    let mean = get_mean(values);
+    let std = values.iter().map(|r| (r - mean) * (r - mean)).sum::<f64>() / values.len() as f64;
+    std.sqrt()
+}
+
+fn get_data(radii: [Vec<f64>; DATA_LEN]) -> [[f64; 5]; DATA_LEN] {
+    let mut data = from_fn(|_| [0.0; 5]);
+    for (i, values) in radii.iter().enumerate() {
+        let mean = get_mean(&values);
+        let cnt = values.len() as f64;
+        data[i] = [
+            (i * ANGLE_ROTATION + ANGLE_ROTATION / 2) as f64,
+            cnt,
+            mean,
+            cnt * mean,
+            get_std(&values),
+        ]
+    }
+    data
+}
+
+fn parse_run_dir(dir: &Path, cutoff: f64) -> Result<[Vec<f64>; DATA_LEN]> {
+    let dump_input = DumpFile::read(&dir.join("dump.initial"), &[])?;
+    let snap_input = dump_input.get_snapshots()[0];
+    let dump_final = DumpFile::read(&dir.join("dump.final_no_cluster"), &[])?;
+    let snap_final = dump_final.get_snapshots()[0];
+    let center =
+        get_center_pos(&dir.join("cluster_xyz.txt")).context("Failed to get center position")?;
+    info!("cluster center: {center}");
+    let rim = rim_snapshot(snap_input, snap_final, cutoff);
+    info!("rim count: {}", rim.atoms_count);
+    let radii = get_angle_distribution(&rim, center);
+    let dump_rim = DumpFile::new(vec![rim]);
+    dump_rim.save(&dir.join("dump.rim"))?;
+    Ok(radii)
 }
 
 fn main() -> Result<()> {
     env_logger::init();
     let cli = Cli::parse();
-
-    let dump_input = DumpFile::read(&cli.dump_input_file, &[])?;
-    let snapshot_input = dump_input.get_snapshots()[0];
-
-    let dump_final = DumpFile::read(&cli.dump_final_file, &[])?;
-    let snapshot_final = dump_final.get_snapshots()[0];
-
-    let center = get_center_pos(&cli.output_dir.join("cluster_xyz.txt"))
-        .context("Failed to get center position")?;
-    info!("cluster center: {center}");
-    let rim = rim_snapshot(snapshot_input, snapshot_final, cli.cutoff);
-
-    info!("rim count: {}", rim.atoms_count);
-
-    let dump_rim = DumpFile::new(vec![rim]);
-    dump_rim.save(&cli.output_dir.join("dump.rim"))?;
+    let radii = match cli.command {
+        Commands::Single { dir } => parse_run_dir(&dir, cli.cutoff)?,
+        Commands::All { dir } => parse_run_dir(&dir, cli.cutoff)?,
+    };
+    let data = get_data(radii);
+    info!("data: {data:?}");
     Ok(())
 }
