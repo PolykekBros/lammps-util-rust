f626f9cdeacbc6bfcc1cf210964acf07e905da64
Denis Strizhkin <strdenis02@gmail.com>
2025-04-24 15:02:13 +0300

    detect-sputtered: fix usage of cluster ids

--- detect-sputtered/src/main.rs
+++ detect-sputtered/src/main.rs
@@ -44,27 +44,28 @@ fn do_run_dir(run_dir: &Path) -> Result<()> {
     let dump_final = DumpFile::read(&run_dir.join("dump.final"), &[])?;
     let snapshot_final = clusterize_snapshot(dump_final.get_snapshots()[0], 3.0);
     let counts = get_cluster_counts(&snapshot_final);
-    let sputter_ids = counts
+    let cluster_ids = counts
         .into_iter()
         .filter(|(_, count)| *count < 1000)
         .map(|(id, _)| id)
         .collect::<Vec<_>>();
-    let ids = snapshot_final.get_property("id");
+    let clusters = snapshot_final.get_property("cluster");
     let mut sputter_indices = Vec::new();
     let mut no_sputter_indices = Vec::new();
-    for (i, id) in ids
+    for (i, cluster) in clusters
         .iter()
         .map(|id| *id as usize)
         .enumerate()
         .take(snapshot_final.atoms_count)
     {
-        if sputter_ids.contains(&id) {
+        if cluster_ids.contains(&cluster) {
             sputter_indices.push(i);
         } else {
             no_sputter_indices.push(i);
         }
     }
     let snapshot_sputter = copy_snapshot_with_indices(&snapshot_final, sputter_indices.into_iter());
+    println!("sputtered: {}", snapshot_sputter.atoms_count);
     let dump_sputter = DumpFile::new(vec![snapshot_sputter]);
     dump_sputter.save(&run_dir.join("dump.sputter"))?;
     let snapshot_no_sputter =
