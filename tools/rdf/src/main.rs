cb957bd836f9936aa8a7e065d96496225f9d7c74
Denis Strizhkin <strdenis02@gmail.com>
2026-03-29 14:30:11 +0300

    tools/rdf: clippy fixes

--- tools/rdf/src/main.rs
+++ tools/rdf/src/main.rs
@@ -1,4 +1,15 @@
+#![deny(
+    clippy::all,
+    clippy::correctness,
+    clippy::suspicious,
+    clippy::complexity,
+    clippy::perf,
+    clippy::style,
+    clippy::pedantic
+)]
 #![allow(clippy::cast_precision_loss)]
+#![allow(clippy::cast_sign_loss)]
+#![allow(clippy::cast_possible_truncation)]
 use clap::Parser;
 use kd_tree::KdTree3;
 use lammps_files::{Dump, Snapshot};
@@ -13,7 +24,7 @@ struct Cli {
     dump_file: PathBuf,
 
     /// types to compute rdf for - '1,2' -> 1-1 + 1-2 + 2-2 (all if none)
-    #[arg(short, long, value_delimiter = ',')]
+    #[arg(short = 'T', long, value_delimiter = ',')]
     types: Vec<usize>,
 
     #[arg(short, long)]
@@ -47,68 +58,63 @@ fn make_table<'a>(
 }
 
 fn normalize_g<'a>(
+    g: &mut Vec<f64>,
     bins: impl IntoIterator<Item = &'a [f64; 2]>,
-    g: impl IntoIterator<Item = usize>,
     snapshot: &Snapshot,
-) -> Vec<f64> {
+) {
     let vol = snapshot.get_symbox().bbox.volume();
     let num = snapshot.get_atoms_count() as f64;
     let rho = num / vol;
-    iter::zip(bins, g)
-        .map(|([lo, hi], n)| {
-            let vshell = 4.0 / 3.0 * std::f64::consts::PI * (hi.powi(3) - lo.powi(3));
-            let nnorm = rho * vshell;
-            n as f64 / (nnorm * num)
-        })
-        .collect()
+    iter::zip(g, bins).for_each(|(n, [lo, hi])| {
+        let vshell = 4.0 / 3.0 * std::f64::consts::PI * (hi.powi(3) - lo.powi(3));
+        let nnorm = rho * vshell;
+        *n /= nnorm * num;
+    });
 }
 
 fn get_rdf_new(snapshot: &Snapshot, n: usize, cutoff: f64, types: &[usize]) -> Vec<f64> {
     let bins_vec = get_bins(cutoff, n);
+    assert_eq!(bins_vec.len(), n * 2);
     let (bins, rem) = bins_vec.as_chunks::<2>();
     assert_eq!(rem.len(), 0);
     assert_eq!(bins.len(), n);
-    assert_eq!(bins.len(), n * 2);
     let mut coords = xyz_vec_from_snapshot(snapshot);
     XYZ::get_supercell_coords(&mut coords, snapshot.get_symbox(), cutoff);
     let kdtree = kd_tree::KdTree::build_by_ordered_float(coords);
-    match types.len() {
-        0 => {
-            let mut table = make_table(bins, bins.len(), 2);
-            let g = calculate_rdf(snapshot, &kdtree, bins, cutoff, |_| true, |_| true);
+    if types.is_empty() {
+        let mut table = make_table(bins, bins.len(), 2);
+        let g = calculate_rdf(snapshot, &kdtree, bins, cutoff, |_| true, |_| true);
+        assert_eq!(g.len(), bins.len());
+        for (idx, g) in g.into_iter().enumerate() {
+            table[idx * 2 + 1] = g;
+        }
+        table
+    } else {
+        let cols = (0..types.len())
+            .map(|i| (i..types.len()).count())
+            .sum::<usize>();
+        let mut table = make_table(bins, bins.len(), cols);
+        let atypes = snapshot.get_property("type");
+        for (col, (ti, tj)) in types
+            .iter()
+            .enumerate()
+            .flat_map(|(i, ti)| types[i..].iter().map(move |tj| (ti, tj)))
+            .enumerate()
+        {
+            let g = calculate_rdf(
+                snapshot,
+                &kdtree,
+                bins,
+                cutoff,
+                |atom| (atypes[atom.index] as usize).eq(ti),
+                |atom| (atypes[atom.index] as usize).eq(tj),
+            );
             assert_eq!(g.len(), bins.len());
-            for (idx, g) in g.into_iter().enumerate() {
-                table[idx * 2 + 1] = g;
+            for (row, g) in g.into_iter().enumerate() {
+                table[row * cols + col + 1] = g;
             }
-            table
         }
-        _ => {
-            let cols = (0..types.len())
-                .map(|i| (i..types.len()).count())
-                .sum::<usize>();
-            let mut table = make_table(bins, bins.len(), cols);
-            let atypes = snapshot.get_property("type");
-            for (col, (ti, tj)) in types
-                .iter()
-                .enumerate()
-                .flat_map(|(i, ti)| types[i..].iter().map(move |tj| (ti, tj)))
-                .enumerate()
-            {
-                let g = calculate_rdf(
-                    snapshot,
-                    &kdtree,
-                    bins,
-                    cutoff,
-                    |atom| (atypes[atom.index] as usize).eq(ti),
-                    |atom| (atypes[atom.index] as usize).eq(tj),
-                );
-                assert_eq!(g.len(), bins.len());
-                for (row, g) in g.into_iter().enumerate() {
-                    table[row * cols + col + 1] = g;
-                }
-            }
-            todo!()
-        }
+        todo!()
     }
 }
 
@@ -124,17 +130,18 @@ where
     F1: Fn(&&XYZ) -> bool,
     F2: Fn(&&XYZ) -> bool,
 {
-    let g = kdtree
+    let mut g = kdtree
         .items()
         .iter()
         .filter(|atom| !atom.is_ghost)
         .filter(atom_1_filter)
         .map(|atom| calculate_rdf_hist(kdtree, bins, cutoff, atom, &atom_2_filter))
-        .fold(Vec::<usize>::new(), |mut g, part_g| {
+        .fold(Vec::new(), |mut g, part_g| {
             iter::zip(&mut g, part_g).for_each(|(g, p_g)| *g += p_g);
             g
         });
-    normalize_g(bins, g, snapshot)
+    normalize_g(&mut g, bins, snapshot);
+    g
 }
 
 fn calculate_rdf_hist<F>(
@@ -143,7 +150,7 @@ fn calculate_rdf_hist<F>(
     cutoff: f64,
     atom: &XYZ,
     atom_2_filter: F,
-) -> Vec<usize>
+) -> Vec<f64>
 where
     F: Fn(&&XYZ) -> bool,
 {
@@ -159,9 +166,9 @@ where
             let hi_sq = hi.powi(2);
             d_sq.iter()
                 .filter(|&&d_sq| d_sq >= lo_sq && d_sq < hi_sq && d_sq != 0.0)
-                .count()
+                .count() as f64
         })
-        .collect::<Vec<_>>()
+        .collect()
 }
 
 fn print_table<T, H>(table: &[T], header: &[H], rows: usize, cols: usize)
@@ -205,22 +212,21 @@ where
 }
 
 fn make_rdf_table_header(types: &[usize]) -> Vec<String> {
-    match types.len() {
-        0 => vec!["r".to_string(), "g(r)".to_string()],
-        _ => {
-            let mut v = Vec::new();
-            v.push("r".to_string());
-            for s in types
-                .iter()
-                .enumerate()
-                .flat_map(|(i, ti)| types[i..].iter().map(move |tj| (ti, tj)))
-                .map(|(ti, tj)| format!("g({ti}-{tj})(r)"))
-            {
-                v.push(s);
-            }
-
-            v
+    if types.is_empty() {
+        vec!["r".to_string(), "g(r)".to_string()]
+    } else {
+        let mut v = Vec::new();
+        v.push("r".to_string());
+        for s in types
+            .iter()
+            .enumerate()
+            .flat_map(|(i, ti)| types[i..].iter().map(move |tj| (ti, tj)))
+            .map(|(ti, tj)| format!("g({ti}-{tj})(r)"))
+        {
+            v.push(s);
         }
+
+        v
     }
 }
 
